80070298fdfde80aa0d2580ccdedd24ade3d6388
YiQiu <binzixiang@163.com>
2024-08-20 12:34:35 +0800

    GameSpace: Introduce Quick Start app feature
    * tool for launching apps in freeform mode while gaming etc.
    
    Change-Id: Ib4e3e62ae1167bafb38ff6e7032c809c3b3bec0e
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Co-Authored-By: minaripenguin <110431802+minaripenguin@users.noreply.github.com>

--- app/src/main/java/io/chaldeaprjkt/gamespace/settings/SettingsFragment.kt
+++ app/src/main/java/io/chaldeaprjkt/gamespace/settings/SettingsFragment.kt
@@ -1,6 +1,7 @@
 /*
  * Copyright (C) 2021 Chaldeaprjkt
  * Copyright (C) 2022-2024 crDroid Android Project
+ *               2023-2024 the risingOS Android Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -22,6 +23,13 @@ import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import dagger.hilt.android.AndroidEntryPoint
 import io.chaldeaprjkt.gamespace.R
+<<<<<<< HEAD
+=======
+import io.chaldeaprjkt.gamespace.preferences.AppListPreferences
+import io.chaldeaprjkt.gamespace.preferences.appselector.AppSelectorActivity
+import io.chaldeaprjkt.gamespace.preferences.QuickStartAppPreference
+import io.chaldeaprjkt.gamespace.preferences.QuickStartAppPreferenceDialogFragment
+>>>>>>> c96667a ( GameSpace: Introduce Quick Start app feature)
 
 @AndroidEntryPoint(PreferenceFragmentCompat::class)
 class SettingsFragment : Hilt_SettingsFragment() {
@@ -36,5 +44,15 @@ class SettingsFragment : Hilt_SettingsFragment() {
 
     override fun onResume() {
         super.onResume()
+    }
+
+    override fun onDisplayPreferenceDialog(preference: Preference) {
+        if (preference is QuickStartAppPreference) {
+            val dialogFragment = QuickStartAppPreferenceDialogFragment.newInstance(preference.key)
+            dialogFragment.setTargetFragment(this, 0)
+            dialogFragment.show(parentFragmentManager, "QuickStartAppPreferenceDialogFragment")
+        } else {
+            super.onDisplayPreferenceDialog(preference)
+        }
     }
 }
